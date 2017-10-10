fe39dc83d434903a22ed2fdacfa3693d62827f16
candy <1964646121@qq.com>
2017-10-10 13:34:50 +0800

    for practice

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class=page>
+      <header>
+        <Topbar/>
+      </header>
+      <main>
+        <ResumeEditor/>
+        <ResumePreview/>
+      </main>
+  </div>
+</template>
+
+<script>
+
+  import './assets/reset.css';
+  // import 'normalize.css/normalize.css';
+  import Topbar from './components/Topbar';
+  import ResumeEditor from './components/ResumeEditor';
+  import ResumePreview from './components/ResumePreview';
+
+  export default {
+    name: 'app',
+    data: function(){
+      return {
+       
+      }
+    },
+    components:{Topbar,ResumePreview,ResumeEditor},
+  }
+</script>
+
+<style>
+  .page{
+    min-width:1024px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #eaebec;
+  }
+  .page>main{
+    flex-grow: 1;
+  }
+  .page>main{
+    min-width: 1024px;
+    max-width: 1440px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    width:100%;   /*不加的话，main虽然居中了，但是宽度变成1024，而非1440*/
+    align-self: center;
+    justify-content: space-between;
+    padding:0 16px;
+  }
+
+  #resumeEditor{
+    width:35%;  
+    background: #444;
+  }
+  #resumePreview{
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #777;
+  }
+</style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="topbar">
+		<div class="wrapper">
+			<span class="logo">resumer</span>
+		</div>
+	</div>
+
+</template>
+<script>
+	export default{
+		name:"topBar",
+
+	}
+</script>
+
+<style>
+	#topbar{
+		background: #fff;
+		box-shadow: 0 1px 3px 0 rgba(0,0,0,.25);
+		height: 64px;
+	}
+	#topbar>.wrapper{
+		min-width: 1024px;
+		max-width: 1440px;
+		margin:0 auto;
+		padding:0 16px;
+	}
+	#topbar .logo{
+		font-size: 24px;
+		color:#000;
+	}
+</style>
