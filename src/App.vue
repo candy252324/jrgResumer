80410413b62ebdca01c68bdde05fd21985b365a5
candy <1964646121@qq.com>
2017-10-10 16:17:35 +0800

    完成切换和预览

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@
   import Topbar from './components/Topbar';
   import ResumeEditor from './components/ResumeEditor';
   import ResumePreview from './components/ResumePreview';
+  import icons from './assets/icons'
 
   export default {
     name: 'app',
@@ -26,31 +27,41 @@
       }
     },
     components:{Topbar,ResumePreview,ResumeEditor},
+    created(){
+      // 在<body>后插入svg标签
+      document.body.insertAdjacentHTML('afterbegin',icons);   
+      //insertHtml(where,el,html),在指定的地方插入html标签语句
+      //where：插入位置。包括beforeBegin,beforeEnd,afterBegin,afterEnd。
+      //el：用于参照插入位置的html元素对象
+      //html：要插入的html代码
+
+    }
   }
 </script>
 
-<style>
+<style lang="scss">
   .page{
     min-width:1024px;
     height: 100vh;
     display: flex;
     flex-direction: column;
     background: #eaebec;
+    >main{
+      flex-grow: 1;
+    }
+    >main{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      width:100%;   /*不加的话，main虽然居中了，但是宽度变成1024，而非1440*/
+      align-self: center;
+      justify-content: space-between;
+      padding:0 16px;
+      }
   }
-  .page>main{
-    flex-grow: 1;
-  }
-  .page>main{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    display: flex;
-    width:100%;   /*不加的话，main虽然居中了，但是宽度变成1024，而非1440*/
-    align-self: center;
-    justify-content: space-between;
-    padding:0 16px;
-  }
+ 
 
   #resumeEditor{
     width:35%;  
@@ -60,5 +71,12 @@
     flex-grow: 1;
     margin-left: 16px;
     background: #777;
+  }
+  svg.icon{
+    height:1em;
+    width:1em;
+    fill:currentColor;   //常规的HTML-CSS中，是没有fill属性的。fill属性只在XML-SVG格式中存在
+    vertical-align:-0.1em;
+    font-size:16px;
   }
 </style>
--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,19 +1,128 @@
 <template>
 	<div id="resumeEditor">
-		edit
+		<nav>
+			<ol>
+				<li v-for="(item,index) in resume.config"
+					:class="{active:item.field===selected}"
+					@click="selected=item.field">
+					<svg class="icon">
+						<use :xlink:href="`#icon-${item.icon}`"></use>
+					</svg>
+				</li>
+			</ol>
+		</nav>
+		<ol class="panels">
+			<li v-for="item in resume.config"
+				v-show="item.field===selected">
+				<!-- resume 属性同时支持数组和对象 -->
+				<div v-if="resume[item.field] instanceof Array">
+					<div class="subitem" v-for="subitem in resume[item.field]">
+						<div class="resumeField" v-for="(value,key) in subitem">
+							<label for="">{{key}}</label>
+							<input type="text" v-model="resume[item.field][key]">
+						</div>
+						<hr>
+					</div>
+				</div>
+				<div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
+		            <label> {{key}} </label>
+		            <input type="text" v-model="resume[item.field][key]">
+		        </div>
+			</li>
+		</ol>
 	</div>
 </template>
 
 <script>
 	export default{
 		name:"ResumeEditor",
+		data(){
+			return{
+				selected:"profile",
+				resume:{
+					config:[
+						{ field: 'profile', icon: 'id' },
+                        { field: 'work history', icon: 'work' },
+                        { field: 'education', icon: 'book' },
+                        { field: 'projects', icon: 'heart' },
+                        { field: 'awards', icon: 'cup' },
+                        { field: 'contacts', icon: 'phone' },
+					],
+					profile:{
+						name:'',
+						city:'',
+						title:'',
+					},
+					'work history':[
+						{company: 'AL', content: '我的第二份工作是'},
+            			{company: 'TX', content: '我的第一份工作是'},
+					],
+					education:[],
+					projects:[],
+					awards:[],
+					contacts:[],
+				}
+			}
+		}
 
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
 	#resumeEditor{
 		background: #fff;
 		box-shadow: 0 1px 3px 0 rgba(0,0,0,.25);
+		display:flex;
+		flex-direction:row;
+		overflow:auto;
+		>nav{
+			width:80px;
+			background:black;
+			color:#fff;
+			>ol{
+				list-style:none;
+				>li{
+					height:48px;
+					display:flex;
+					justify-content:center;
+					align-items:center;
+					cursor:pointer;
+					&.active{
+						background:#fff;
+						color:#000;
+					}
+				}
+			}
+		}
+		>.panels{
+			flex-grow:1;
+			list-style:none;
+			>li{
+				padding:24px;
+			}
+
+		}
+		.resumeField{
+			>label{
+				display:block;
+			}
+			input[type=text]{
+				margin:16px 0;
+				border:1px solid #ddd;
+				box-shadow:inset 0 1px 3px 0 rgba(0,0,0,.25);
+				width:100%;
+				height:40px;
+				padding:0 8px;
+			}
+		}
+		svg.icon{
+			width:24px;
+			height:24px;
+		}
+		hr{
+ 		   border: none;
+ 		   border-top: 1px solid #ddd;
+ 		   margin: 24px 0;
+ 		}
 	}
 </style>
